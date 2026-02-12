10fa3ad71d016706979b49ea1788364fdbfddd3f
Harsh0604-2006 <harshmanoj0604@gmail.com>
2026-02-13 00:17:03 +0530

    adding bubblesort algo

---
+++ BubbleSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void bubbleSort(vector<int> arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+
+        arr.push_back(a);
+    }
+
+    bubbleSort(arr, n);
+}
